1427c0d840f5baea4bfbdae03a3a21de77147c24
HTP113 <pichey2024@gmail.com>
2023-04-10 20:43:29 +0700

    add

---
+++ src/main/java/com/codegym/Introduction_to_java/practice/SystemTime/Main.java
@@ -0,0 +1,10 @@
+package com.codegym.Introduction_to_java.practice.SystemTime;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("this is: " + now);
+    }
+}
